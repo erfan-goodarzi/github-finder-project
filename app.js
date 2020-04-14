04fb3de301cb1982f0fd77ef4bccd9884865f3b0
erfan goodarzi <erfangood88@gmail.com>
2020-04-14 02:27:05 -0700

    add loading

--- app.js
+++ app.js
@@ -9,18 +9,28 @@ input.addEventListener('keyup', e=>{
     }
 })
 btnsearch.addEventListener('click' , e =>{
+    document.querySelector('.loaded').style.display = 'block';
+    setTimeout(porfilegithub,3000)
+})
+
+function porfilegithub(){
     let search = input.value;
     if(search === ''){
         ui.showalert('please search the user' , 'alert alert-danger');
+        document.querySelector('.loaded').style.display = 'none';
+        
         
     }else{
         github.usergit(search).then(data =>{
             if(data.profile.message){
                 ui.showalert('user not found' , 'alert alert-warning');
+                document.querySelector('.loaded').style.display = 'none';
                 ui.clearprofile();
             }else{
+                document.querySelector('.loaded').style.display = 'none';
                 ui.progithub(data.profile);
+                
             }
         })
     }
-})
+}
--- ui.js
+++ ui.js
@@ -6,34 +6,31 @@ class UI {
     progithub(user) {
         this.profile.innerHTML = `
         <div class="profile mb-3 mt-5">
-        <div class="container">
-        
-        <div class="pro" style="width: 74%;" >
-        
-        <img src="${user.avatar_url}" alt="img" class="mb-3 img-porfile">
-        <a href="${user.html_url}" class="btn btn-primary" style="width:58%;" target="_blank">go to github user</a>
-    
-        </div>
-       
-            
-            <ul class="list-group mt-4" style="margin-left: 0rem;
-            width: 43%;">
-            <li class="list-group-item">Username : ${user.login}</li>
-            <li class="list-group-item">Name : ${user.name}</li>
-            <li class="list-group-item">location : ${user.location}</li>
-            <li class="list-group-item">profile repos : ${user.public_repos}</li>
-            <li class="list-group-item">profile followers : ${user.followers}</li>
-            <li class="list-group-item">profile following : ${user.following}</li>
-            <li class="list-group-item">type : ${user.type}</li>
-            </ul>
-        
-    
-        </div>
-        
-    </div>
-    
-    
+                <div class="container">
+                    <div class="d-flex">
+                        
+                    <div class="pro" style="width: 74%;">
 
+                        <img src="${user.avatar_url}" alt="img" class="mb-3 img-porfile">
+                        <a href="${user.html_url}" class="btn btn-primary" style="width:58%;" target="_blank">go to
+                            github user</a>
+
+                    </div>
+
+
+                    <ul class="list-group mt-4" style="margin-left: -17rem;
+                    width: 44%;">
+                        <li class="list-group-item">Username : ${user.login}</li>
+                        <li class="list-group-item">Name : ${user.name}</li>
+                        <li class="list-group-item">location : ${user.location}</li>
+                        <li class="list-group-item">profile repos : ${user.public_repos}</li>
+                        <li class="list-group-item">profile followers : ${user.followers}</li>
+                        <li class="list-group-item">profile following : ${user.following}</li>
+                        <li class="list-group-item">type : ${user.type}</li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
 
         `;
     }
@@ -49,5 +46,10 @@ class UI {
         alert.textContent = msg;
 
         searching.insertBefore(alert, search);
+        setTimeout(this.removealert,2000)
+        
+    }
+    removealert(){
+        document.querySelector('.alert').remove();
     }
 }
